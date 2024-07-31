f1465ba190dd5be18e375adb6d0f36eca313a484
Elisa Bugani <elisa.bugani@gmail.com>
2024-07-31 12:51:35 +0200

    minor chnage for repr

--- downstream/Chip/filter_peaks.histone_marks.R
+++ downstream/Chip/filter_peaks.histone_marks.R
@@ -39,10 +39,11 @@ SEED <- 4321
 set.seed(SEED)
 
 MARKERS <- c("CtIP", "GRHL")
+location <- "local" # 'local' or 'hpc'
 
 # Paths CtIP/GRHL peaks
-path_input_peaks <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05//macs2/")
-path_peaks_merged <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05//downstream/peaks_union/")
+path_input_peaks <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/macs2/")
+path_peaks_merged <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
 
 # Paths histone marks 
 path_peaks_k4me3 <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/macs2/MCF10A_S48547_230308_hg19_H3K4me3_peaks.narrowPeak")
@@ -51,93 +52,25 @@ path_peaks_k27ac <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/d
 
 # Paths output 
 path_output_k4me3 <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/macs2/")
-path_output_merged <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05//downstream/peaks_union/")
+path_output_merged <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
 
-location <- "local"
 if(location == "hpc"){
   path_input_peaks <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/bwa/mergedLibrary/macs2/narrowPeak/")  
-  path_output_merged <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/peaks_union/")
+  path_peaks_merged <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
+  
+  path_peaks_k4me3 <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/bwa/mergedLibrary/macs2/narrowPeak/MCF10A_S48547_230308_hg19_H3K4me3_peaks.narrowPeak")
+  path_peaks_k4me1 <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/bwa/mergedLibrary/macs2/narrowPeak/MCF10A_S47229_SCR_221230_hg19_H3K4me1_peaks.narrowPeak")
+  path_peaks_k27ac <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/bwa/mergedLibrary/macs2/narrowPeak/MCF10A_S43557_SCR_220801_hg19_H3K27ac_peaks.narrowPeak")
+  
+  path_output_k4me3 <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/HISTONE_MARKS/bwa/mergedLibrary/macs2/narrowPeak/")
+  path_output_merged <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
 }
-
 
 # metric1 and metric2 could be pValue and qValue
 narrowPeak_colnames <- c("chrom", "start", "end", "name", "score", "strand", 
                          "signal_value", "metric1", "metric2", "summit")
 narrowPeak_colnames_nostrand <- c("chrom", "start", "end", "name", "score", 
                                   "signal_value", "metric1", "metric2", "summit")
-
-
-##
-
-
-## Plot tot. number of peaks x Chip + Peaks annotations x Chip 
-
-# Total number of peaks
-num_peaks <- read_tsv(fs::path(path_input_peaks, "qc/macs2_peak.summary.txt")) %>% suppressMessages()
-num_peaks[num_peaks$measure == "fold", ] %>% 
-  ggplot(., aes(x = sample, y = num_peaks, fill = sample))+
-  geom_bar(stat = "identity")+
-  theme_light()+
-  labs(title = "Number of peaks", y = "n. peaks")+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5), 
-        legend.position = "none")
-
-# Peaks annotations 
-anno <- read_tsv(fs::path(path_input_peaks, "qc/macs2_annotatePeaks.summary.txt")) %>% suppressMessages()
-anno[is.na(anno)] <- 0
-anno <- anno %>% mutate(., tot = Intergenic+TTS+exon+intron+`promoter-TSS`)
-
-anno %>% mutate(., across(Intergenic:`promoter-TSS`, ~ . / tot)) %>% 
-  dplyr::select(., -tot) %>%
-  pivot_longer(-sample) %>% 
-  ggplot(., aes(x=sample, y = value, fill = name))+
-  geom_bar(stat = "identity", position = "stack")+
-  theme_light()+
-  labs(title = "Peaks annotation", y = "")+
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5))
-
-
-##
-
-
-# Filter out potential enhancer peaks according to annotation
-anno_of_int <- tolower(c("Intergenic", "intron")) # leaving out: TSS, Promoter, exon
-
-peaks_all <- list.files(path_input_peaks)[endsWith(list.files(path_input_peaks), "peaks.narrowPeak")]
-peaks_anno_all <- list.files(path_input_peaks)[endsWith(list.files(path_input_peaks), "annotatePeaks.txt")]
-
-for(marker in MARKERS){
-  cat("\n")
-  print(marker)
-  
-  peaks_anno_marker <- grep(marker, peaks_anno_all, value = T)
-  print(peaks_anno_marker)
-  
-  for(anno_file in peaks_anno_marker){
-    sample_name <- str_split(anno_file, "\\.")[[1]][1]
-    
-    peaks_anno <- read_tsv(fs::path(path_input_peaks, anno_file)) %>% suppressMessages()
-    colnames(peaks_anno)[1] <- "PeakID"
-    peaks_anno$simple_annotation <- str_split(peaks_anno$Annotation, pattern = c("-"), simplify = T)[,1] %>% str_split(., pattern=" ", simplify = T) %>% .[,1]
-    
-    # Select only peaks that could represent enhancers and filter bed file 
-    peaks_anno_filt <- peaks_anno %>% dplyr::filter(., tolower(simple_annotation) %in% anno_of_int)
-    
-    peaks_bed <- read_tsv(fs::path(path_input_peaks, peaks_all[startsWith(peaks_all, sample_name)]), 
-                          col_names = narrowPeak_colnames) %>% suppressMessages() 
-    peaks_bed_filt <- peaks_bed[peaks_bed$name %in% peaks_anno_filt$PeakID, ]
-    print(dim(peaks_bed_filt)[1] == dim(peaks_anno_filt)[1])
-    
-    #write_tsv(peaks_bed_filt, fs::path(path_input_peaks, paste0(sample_name, ".filt.narrowPeak" )), col_names = F)
-  }
-}
-
-
-##
-
-
-# --> Merge peaks with bedtools 
-# TODO: add path to script
 
 
 ##
