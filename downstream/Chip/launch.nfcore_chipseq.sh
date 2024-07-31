521be3be37218b49de593d40a544cd074f01eaff
Elisa Bugani <elisa.bugani@gmail.com>
2024-07-31 12:07:30 +0200

    minor change for repr

--- downstream/Chip/launch.nfcore_chipseq.sh
+++ downstream/Chip/launch.nfcore_chipseq.sh
@@ -12,12 +12,14 @@ source ~/.bashrc
 mamba activate nf_env
 
 IN_DIR='/hpcnfs/scratch/PGP/ebugani/fragile_enhancers_clinical/run_nf/'
-OUT_DIR='/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/'
+OUT_DIR='/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/'
 INPUT_SHEET='/hpcnfs/scratch/PGP/ebugani/fragile_enhancers_clinical/chip_marta_samplesheet.csv'
 
-cd $IN_DIR
+if [ ! -d $OUT_DIR ]; then
+	mkdir -p $OUT_DIR
+fi 
 
-#export TOWER_ACCESS_TOKEN=eyJ0aWQiOiA5NjQ2fS44NjhiOTUzN2FhY2YxNGFlNDVmOTA3YmY0OWIxYzA4YTJiMmI1ZmZh
+cd $IN_DIR
 
 nextflow run nf-core/chipseq -r 2.0.0 \
     --input $INPUT_SHEET \
@@ -26,8 +28,9 @@ nextflow run nf-core/chipseq -r 2.0.0 \
     --macs_gsize 2864785220 \
     --narrow_peak \
     -profile singularity \
-    -with-tower 
-
+    -with-tower \
+    --macs_pvalue 0.01 
+   
 # blacklisted regions for the specified genome are bundled with the pipeline 
 
 
