88e8fc23162fc4924529349a8e9d3e23d40b6942
Elisa Bugani <elisa.bugani@gmail.com>
2024-07-31 16:05:28 +0200

    .

--- downstream/Chip/7.compute_overlaps.R
+++ downstream/Chip/7.compute_overlaps.R
@@ -7,13 +7,19 @@ library(GenomicRanges)
 
 
 MARKERS <- c("CtIP", "GRHL")
+location <- "local" # 'local' or 'hpc'
 
 path_input_peaks <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/macs2/")
 path_peaks_union1 <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
-path_peaks_union2 <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL/downstream/peaks_union/")
 path_output_merged <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHLq05/downstream/peaks_union/")
-
 path_old_enhancers <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/others/")
+
+if(location == "hpc"){
+  path_input_peaks <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/bwa/mergedLibrary/macs2/narrowPeak/")
+  path_peaks_union1 <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
+  path_output_merged <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/")
+  path_old_enhancers <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/others/")
+}
 
 # metric1 and metric2 could be pValue and qValue
 narrowPeak_colnames <- c("chrom", "start", "end", "name", "score", "strand", 
@@ -38,14 +44,9 @@ new1 <- list("CtIP" = read_tsv(fs::path(path_peaks_union1, "merged_peaks.hg19_Ct
                                col_names = narrowPeak_colnames_nostrand),
              "GRHL" = read_tsv(fs::path(path_peaks_union1, "merged_peaks.hg19_GRHL.filtered_K27ac_k4me1_k4me3.hq_signal.new_summit.narrowPeak"), 
                                col_names = narrowPeak_colnames_nostrand))
-new2 <- list("CtIP" = read_tsv(fs::path(path_peaks_union2, "merged_peaks.hg19_CtIP.filtered_K27ac_k4me1_k4me3.new_summit.narrowPeak"), 
-                              col_names = narrowPeak_colnames_nostrand),
-            "GRHL" = read_tsv(fs::path(path_peaks_union2, "merged_peaks.hg19_GRHL.filtered_K27ac_k4me1_k4me3.new_summit.narrowPeak"), 
-                              col_names = narrowPeak_colnames_nostrand))
 
 old_gr_all <- list()
 new1_gr_all <- list()
-new2_gr_all <- list()
 
 extend_summits <- T
 WIN <- 1000
@@ -58,10 +59,7 @@ for(marker in MARKERS){
   new1_gr <- new1[[marker]] %>% makeGRangesFromDataFrame(., keep.extra.columns=T)
   seqlevels(new1_gr) <- paste0("chr", seqlevels(new1_gr))
   new1_gr_all[[marker]] <- new1_gr
-  new2_gr <- new2[[marker]] %>% makeGRangesFromDataFrame(., keep.extra.columns=T)
-  seqlevels(new2_gr) <- paste0("chr", seqlevels(new2_gr))
-  new2_gr_all[[marker]] <- new2_gr
-  
+
   if(extend_summits ==T){
     start(old_gr) <- start(old_gr) - WIN
     end(old_gr) <- end(old_gr) + WIN
@@ -77,15 +75,10 @@ for(marker in MARKERS){
   
   ovrlps1 <- !is.na(new1_gr %>% findOverlaps(., old_gr, select = "arbitrary"))
   ovrlps1v <- !is.na(old_gr %>% findOverlaps(., new1_gr, select = "arbitrary"))
-  ovrlps2 <- !is.na(new2_gr %>% findOverlaps(., old_gr, select = "arbitrary"))
-  ovrlps2v <- !is.na(old_gr %>% findOverlaps(., new2_gr, select = "arbitrary"))
   
   print(paste0("Percentage of ", marker, " peaks q05 overlapping with old ones - ", 
         sum(ovrlps1), " over ", length(new1_gr), " : ~", (sum(ovrlps1) / length(new1_gr)) %>% round(.,2) *100, "%"))
   print(paste0("Viceversa - old overlaps with new1 - : ", sum(ovrlps1v), " over ", length(old_gr), " : ", (sum(ovrlps1v) / length(old_gr)) %>% round(.,2) *100, "%"))
-  print(paste0("Percentage of ", marker, " peaks p01 overlapping with old ones - ", 
-               sum(ovrlps2), " over ", length(new2_gr), " : ~", (sum(ovrlps2) / length(new2_gr)) %>% round(.,2) *100, "%"))
-  print(paste0("Viceversa - old overlaps with new2 - : ", sum(ovrlps2v), " over ", length(old_gr), " : ", (sum(ovrlps2v) / length(old_gr)) %>% round(.,2) *100, "%"))
 }
 
 
@@ -105,15 +98,6 @@ backw <- !is.na(findOverlaps(new1_gr_all$GRHL, new1_gr_all$CtIP, select = "arbit
 print(paste0("NEW1 CtIP enhancers overlapping with GRHL2 enhancers: ", 
              sum(forw), " over ", length(forw), " - ", (sum(forw) / length(forw)) %>% round(.,2)*100, "%" ))
 print(paste0("NEW1 GRHL enhancers overlapping with CtIP enhancers: ", 
-             sum(backw), " over ", length(backw), " - ", (sum(backw) / length(backw)) %>% round(.,2)*100, "%" ))
-
-
-# new2 - p-value < 0.01
-forw <- !is.na(findOverlaps(new2_gr_all$CtIP, new2_gr_all$GRHL, select = "arbitrary"))
-backw <- !is.na(findOverlaps(new2_gr_all$GRHL, new2_gr_all$CtIP, select = "arbitrary"))
-print(paste0("NEW2 CtIP enhancers overlapping with GRHL2 enhancers: ", 
-             sum(forw), " over ", length(forw), " - ", (sum(forw) / length(forw)) %>% round(.,2)*100, "%" ))
-print(paste0("NEW2 GRHL enhancers overlapping with CtIP enhancers: ", 
              sum(backw), " over ", length(backw), " - ", (sum(backw) / length(backw)) %>% round(.,2)*100, "%" ))
 
 
