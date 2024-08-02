858f7a4fbfaeec6256b8f025288eb04dc8c48970
Elisa Bugani <elisa.bugani@gmail.com>
2024-08-02 12:03:24 +0200

    minor changes for repr

--- downstream/ICGC/0.Annotate_enhancers_to_clusters.R
+++ downstream/ICGC/0.Annotate_enhancers_to_clusters.R
@@ -9,6 +9,13 @@ path_peaks_union_ctip <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clini
 path_peaks_union_grhl <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/merged_peaks.hg19_GRHL.filtered_K27ac_k4me1_k4me3.hq_signal.new_summit.narrowPeak")
 path_results <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/")
 
+if(location == "hpc"){
+  path_clustered_peaks <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/deepTools/heatmap.CtIP_and_GRHL_bw.CtIP_and_GRHL_regions.hq_signal.from_mean.clustered.bed")
+  path_peaks_union_ctip <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/merged_peaks.hg19_CtIP.filtered_K27ac_k4me1_k4me3.hq_signal.new_summit.narrowPeak")
+  path_peaks_union_grhl <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/peaks_union/merged_peaks.hg19_GRHL.filtered_K27ac_k4me1_k4me3.hq_signal.new_summit.narrowPeak")
+  path_results <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/")
+}
+
 MARKERS <- c("CtIP", "GRHL") 
 narrowPeak_colnames_nostrand <- c("chrom", "start", "end", "name", "score", 
                                   "signal_value", "metric1", "metric2", "summit")
