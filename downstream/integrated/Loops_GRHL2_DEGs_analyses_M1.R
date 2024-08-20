806fa5f13ba2a144f7e59ce5c9b70e5463fc6167
Elisa Bugani <elisa.bugani@gmail.com>
2024-08-20 16:17:48 +0200

    minor

--- downstream/integrated/Loops_GRHL2_DEGs_analyses_M1.R
+++ downstream/integrated/Loops_GRHL2_DEGs_analyses_M1.R
@@ -4,12 +4,12 @@ library(GenomicRanges)
 library(viridis)
 library(gridExtra)
 
-source("/Users/ieo6983/Desktop/fragile_enhancer_clinical/utils/loops_functions.R")
 
 ### Hi-ChIP Loops ###
 kb <- 2
 location <- "local" # 'local' or 'hpc'
 
+source("/Users/ieo6983/Desktop/fragile_enhancer_clinical/utils/loops_functions.R")
 path_hichip <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/HiChip/filtered_loops/")
 path_enhancers <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/")
 path_enhancers_ctip <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/CtIP_enh.hq_signal.clustered.tsv")
@@ -24,13 +24,14 @@ path_anno_loops <- fs::path(paste0("/Users/ieo6983/Desktop/fragile_enhancer_clin
 path_results <- fs::path(paste0("/Users/ieo6983/Desktop/fragile_enhancer_clinical/results/integrated/NEW/", kb, "kb"))
 
 if(location == "hpc"){
+  source("/hpcnfs/scratch/PGP/Ciacci_et_al/fragile_enhancer_clinical/utils/loops_functions.R")
   path_hichip <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/HiChip/filtered_loops/")
   path_enhancers <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/")
   path_enhancers_ctip <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/CtIP_enh.hq_signal.clustered.tsv")
   path_enhancers_grhl <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/Chip/Chip_for_clusters/results/CtIP_GRHL_q05/downstream/GRHL_enh.hq_signal.clustered.tsv")
   
-  path_tss <- fs::path("/Users/ieo6983/Desktop/fragile_enhancer_clinical/data/functional_genomics/others/TSSs_elisa/TSSs_from_USCS_hg19_EMSEMBL.tsv")
-  path_degs <- fs::path("/Users/ieo6983/Desktop/expression/DEGs/Df_DEGs.df_LFC_sig.padj_0.05.log2FC_1.Up_and_Down.tsv")
+  path_tss <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/data/functional_genomics/others/TSSs_elisa/TSSs_from_USCS_hg19_EMSEMBL.tsv")
+  path_degs <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/results/expression/RNA_seq/DEGs/Df_DEGs.df_LFC_sig.padj_0.05.log2FC_1.Up_and_Down.tsv")
   path_hallmark <- fs::path("/hpcnfs/scratch/PGP/Ciacci_et_al/results/integrated/NEW/hallmark_gene_sets.h.all.v2023.2.Hs.symbols.gmt")
   
   path_anno_loops <- fs::path(paste0("/hpcnfs/scratch/PGP/Ciacci_et_al/results/integrated/NEW/", kb, "kb/data/"))
